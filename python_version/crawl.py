d6cc02282eeeb6f3afb5c83f57d1a6af1f9c7b1f
David Gengenbach <davidgengenbach@gmx.de>
2017-02-21 17:36:52 +0100

    :)

--- python_version/crawl.py
+++ python_version/crawl.py
@@ -6,6 +6,12 @@ import operator
 import functools
 import os
 
+# These are the icons of the links that get downloaded!
+# You can get the name of such an icon from the moodle course site.
+# The last part of the image src can be added here
+ICON_WHITELIST = ['pdf-24', 'archive-24', 'sourcecode-24', 'mpeg-24', 'powerpoint-24']
+UNUSED_ICONS = []
+
 
 def main():
     parser = argparse.ArgumentParser(description="Download moodle assets")
@@ -13,10 +19,6 @@ def main():
     args = parser.parse_args()
     user, password = helper.get_credentials()
     download_assets(args.url, user, password)
-
-
-def sanitize_title(title):
-    return title.replace(' ', '_').replace('/', '-').replace(':', '-').replace('-', '_')
 
 
 def download_assets(url, username, password, out_folder='out'):
@@ -33,6 +35,8 @@ def download_assets(url, username, password, out_folder='out'):
         else:
             print('\tWill not get downloaded: "{}"'.format(link.text))
 
+    print('\n(The following assets with icon-names will not be downloaded: {})\n'.format(', '.join(UNUSED_ICONS)))
+
     folder_name = out_folder + '/' + title
     if not os.path.exists(folder_name):
         os.mkdir(folder_name)
@@ -40,20 +44,35 @@ def download_assets(url, username, password, out_folder='out'):
     for asset in asset_links_filtered:
         link = asset.attrs['href']
         print('\tDownloading: "{}"'.format(asset.text))
-        download_file(browser, link, folder_name)
+        download_file(browser, link, folder_name, default_filename = sanitize_title(asset.text.replace('Datei', '')))
 
 
 def filter_element(link):
-    ICON_WHITELIST = ['pdf-24', 'archive-24']
-    icon_name = link.select('img')[0].attrs['src'].split('/')[-1]
-    return icon_name in ICON_WHITELIST
+    try:
+        icon_name = link.select('img')[0].attrs['src'].split('/')[-1]
+    except:
+        return False
+    gets_downloaded = icon_name in ICON_WHITELIST
+    if not gets_downloaded:
+        UNUSED_ICONS.append(icon_name)
+    return gets_downloaded
 
 
-def download_file(browser, url, folder):
+def download_file(browser, url, folder, default_filename):
     page = browser.get(url)
-    filename = page.headers['Content-Disposition'].split('"')[-2]
+
+    try:
+        filename = page.headers['Content-Disposition'].split('"')[-2]
+    except:
+        filename = default_filename
+
     with open(folder + '/' + filename, 'wb') as f:
         f.write(page.content)
+
+
+def sanitize_title(title):
+    return title.replace(' ', '_').replace('/', '-').replace(':', '-').replace('-', '_').strip()
+
 
 if __name__ == '__main__':
     main()
