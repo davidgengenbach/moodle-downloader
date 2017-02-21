21f42019e89c23e3fac8e2adb2305538ff1e1be3
David Gengenbach <davidgengenbach@gmx.de>
2017-02-21 17:01:45 +0100

    :)

--- python_version/crawl.py
+++ python_version/crawl.py
@@ -6,52 +6,40 @@ import operator
 import functools
 import os
 
-WHITE_LIST = 'pdf Exercise Teil Präsentation Tutor Uebung Zusatz Übung Lösung Vorlesung Aufzeichnung Multiplizierer Klausur Tutorial History supplementary video pdfs interfaces'.lower(
-).split(' ')
-BLACK_LIST = 'Forum Gruppe Sprechstunden'.lower().split(' ')
-
 
 def main():
     parser = argparse.ArgumentParser(description="Download moodle assets")
     parser.add_argument('--url', default='https://moodle.informatik.tu-darmstadt.de/course/view.php?id=155')
     args = parser.parse_args()
-
-    if not os.path.exists('user.txt'):
-        print(
-            'No "user.txt" in this folder. Please put your username and password on their own line in the "user.txt" file.')
-        exit(1)
-
-    user, password = [x.strip() for x in open('user.txt').read().split() if x.strip() != '']
+    user, password = helper.get_credentials()
     download_assets(args.url, user, password)
-
-
-def download_file(browser, url, folder):
-    page = browser.get(url)
-    filename = page.headers['Content-Disposition'].split('"')[-2]
-    with open(folder + '/' + filename, 'wb') as f:
-        f.write(page.content)
 
 
 def sanitize_title(title):
     return title.replace(' ', '_').replace('/', '-').replace(':', '-').replace('-', '_')
 
 
-def download_assets(url, username, password):
+def download_assets(url, username, password, out_folder='out'):
+    print('Logging in')
     browser = helper.login(url, username, password)
     page = browser.get(url)
     title = sanitize_title(page.soup.find('title').text)
+    print('Retrieving links')
     asset_links = page.soup.select('body .activityinstance a')
     asset_links_filtered = []
     for link in asset_links:
         if filter_element(link):
             asset_links_filtered.append(link)
+        else:
+            print('\tWill not get downloaded: "{}"'.format(link.text))
 
-    folder_name = 'out/' + title
+    folder_name = out_folder + '/' + title
     if not os.path.exists(folder_name):
         os.mkdir(folder_name)
-
+    print('Starting download')
     for asset in asset_links_filtered:
         link = asset.attrs['href']
+        print('\tDownloading: "{}"'.format(asset.text))
         download_file(browser, link, folder_name)
 
 
@@ -59,6 +47,13 @@ def filter_element(link):
     ICON_WHITELIST = ['pdf-24', 'archive-24']
     icon_name = link.select('img')[0].attrs['src'].split('/')[-1]
     return icon_name in ICON_WHITELIST
+
+
+def download_file(browser, url, folder):
+    page = browser.get(url)
+    filename = page.headers['Content-Disposition'].split('"')[-2]
+    with open(folder + '/' + filename, 'wb') as f:
+        f.write(page.content)
 
 if __name__ == '__main__':
     main()
--- python_version/helper.py
+++ python_version/helper.py
@@ -1,4 +1,5 @@
 import mechanicalsoup
+import getpass
 
 def login(url, username, password):
     browser = mechanicalsoup.Browser()
@@ -12,3 +13,33 @@ def login(url, username, password):
     form.select('#password')[0].attrs['value'] = password
     page = browser.submit(form, submit_url)
     return browser
+
+def get_credentials():
+    CREDENTIALS_FILE = 'user.txt'
+    def get_by_file():
+        with open(CREDENTIALS_FILE, 'r') as f:
+            contents = [x.strip() for x in f.readlines() if x.strip() != '']
+            if len(contents) != 2:
+                raise Exception('Error retrieving username/password: File {} does not have two lines. Content: "{}"'.format(CREDENTIALS_FILE, '\n'.join(contents)))
+            return contents
+
+    def ask_user():
+        username = None
+        password = None
+        while username is None:
+            username = input('TUID: ')
+        while password is None:
+            password = getpass.getpass('Password: ')
+        return username, password
+
+    # First try to get the user credentials by file, afterwards by programm arguments
+    try:
+        return get_by_file()
+    except:
+        pass
+
+    try:
+        return ask_user()
+    except:
+        pass
+    raise(Exception('Could not retreive username/password. Are you doing this on purpose?'))
